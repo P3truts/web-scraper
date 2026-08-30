5aa41af6dc9829956d3cd55bd7a84aa0dc7f7ea8
P3truts <obreja.petrut@gmail.com>
2026-08-31 00:43:18 +0300

    D: add main

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-console.log("Hello, World!");
+import { argv } from "node:process";
 
+
+function main() {
+    console.log(argv.length);
+    if (argv.length < 3) {
+        throw Error("Invalid argument - base URL is required!");
+    } else if (argv.length > 3) {
+        throw Error("Invalid argument - only the base URL is required!");
+    }
+    console.log("Crawler runs on domain: ", argv[2]);
+}
+
+main();
