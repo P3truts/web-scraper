6e0f60415ab59d11babfbf51b38385d5fe372fa2
P3truts <obreja.petrut@gmail.com>
2026-08-30 16:20:29 +0300

    B: add extract headings and paragraph

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,26 +1,189 @@
 import { expect, test } from 'vitest'
-import { normalizeURL } from './crawl.js'
+import { getFirstParagraphFromHTML, getHeadingFromHTML, normalizeURL } from './crawl.js'
 
-test('normalizes URL strings', () => {
-  expect(normalizeURL("https://www.boot.dev/blog/path/")).toBe("www.boot.dev/blog/path");
-  expect(normalizeURL("https://www.boot.dev/blog/path")).toBe("www.boot.dev/blog/path");
-  expect(normalizeURL("http://www.boot.dev/blog/path/")).toBe("www.boot.dev/blog/path");
-  expect(normalizeURL("http://www.boot.dev/blog/path")).toBe("www.boot.dev/blog/path");
+
+// normalize
+test('normalizes https URL string with / at the end of path', () => {
+  const URLstring = "https://www.boot.dev/blog/path/";
+  const res = "www.boot.dev/blog/path";
+  expect(normalizeURL(URLstring)).toBe(res);
+})
+
+test('normalizes https URL string without / at end of path', () => {
+  const URLstring = "https://www.boot.dev/blog/path";
+  const res = "www.boot.dev/blog/path";
+  expect(normalizeURL(URLstring)).toBe(res);
+})
+
+test('normalizes http URL string with / at end of path', () => {
+  const URLstring = "http://www.boot.dev/blog/path/";
+  const res = "www.boot.dev/blog/path";
+  expect(normalizeURL(URLstring)).toBe(res);
+})
+
+test('normalizes http URL string without / at end of path', () => {
+  const URLstring = "http://www.boot.dev/blog/path";
+  const res = "www.boot.dev/blog/path";
+  expect(normalizeURL(URLstring)).toBe(res);
 })
 
 test('fail to normalize invalid URL string', () => {
-  expect(() => normalizeURL("5")).toThrow("Invalid URL");
+  const URLstring = "5";
+  const res = "Invalid URL";
+  expect(() => normalizeURL(URLstring)).toThrow(res);
 })
 
 test('fail to normalize missing protocol in URL string', () => {
-  expect(() => normalizeURL("www.abc.com")).toThrow("Invalid URL");
+  const URLstring = "www.abc.com";
+  const res = "Invalid URL";
+  expect(() => normalizeURL(URLstring)).toThrow(res);
 })
 
 test('fail to normalize bad http protocol URL string', () => {
-  expect(() => normalizeURL("hutop://www.abc.com")).toThrow("Invalid protocol! Only accepts 'http' or 'https'!");
+  const URLstring = "hutop://www.abc.com";
+  const res = "Invalid protocol! Only accepts 'http' or 'https'!";
+  expect(() => normalizeURL(URLstring)).toThrow(res);
 })
 
 test('fail to normalize bad http connector URL string', () => {
-  expect(() => normalizeURL("http:/|www.abc.com")).toThrow("Invalid URL");
+  const URLstring = "http:/|www.abc.com";
+  const res = "Invalid URL";
+  expect(() => normalizeURL(URLstring)).toThrow(res);
+})
+
+// heading
+test('gets h1 heading from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <h1>Welcome to Boot.dev</h1>
+      <main>
+        <p>Learn to code by building real projects.</p>
+        <p>This is the second paragraph.</p>
+      </main>
+    </body>
+  </html>`;
+  let res = "Welcome to Boot.dev";
+  expect(getHeadingFromHTML(HTML)).toBe(res);
+})
+
+test('gets h2 heading when h1 is missing from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <h2>Welcome to Boot.dev</h2>
+      <main>
+        <p>Learn to code by building real projects.</p>
+        <p>This is the second paragraph.</p>
+      </main>
+    </body>
+  </html>`;
+  let res = "Welcome to Boot.dev";
+  expect(getHeadingFromHTML(HTML)).toBe(res);
+})
+
+test('gets multiple h1 headings from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <h1>Welcome to Boot.dev</h1>
+      <h1>Welcome again</h1>
+      <main>
+        <p>Learn to code by building real projects.</p>
+        <p>This is the second paragraph.</p>
+      </main>
+    </body>
+  </html>`;
+  let res = "Welcome to Boot.dev, Welcome again";
+  expect(getHeadingFromHTML(HTML)).toBe(res);
+})
+
+test('gets multiple h2 headings from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <h2>Welcome to Boot.dev</h2>
+      <h2>Welcome again</h2>
+      <main>
+        <p>Learn to code by building real projects.</p>
+        <p>This is the second paragraph.</p>
+      </main>
+    </body>
+  </html>`;
+  let res = "Welcome to Boot.dev, Welcome again";
+  expect(getHeadingFromHTML(HTML)).toBe(res);
+})
+
+test('returns empty when headings are missing from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <main>
+        <p>Learn to code by building real projects.</p>
+        <p>This is the second paragraph.</p>
+      </main>
+    </body>
+  </html>`;
+  let res = "";
+  expect(getHeadingFromHTML(HTML)).toBe(res);
+})
+
+// paragraph
+test('gets paragraph from main HTML', () => {
+  let HTML = `<html>
+    <body>
+      <h1>Welcome to Boot.dev</h1>
+      <main>
+        <p>Learn to code by building real projects.</p>
+        <p>This is the second paragraph.</p>
+      </main>
+    </body>
+  </html>`;
+  let res = "Learn to code by building real projects.";
+  expect(getFirstParagraphFromHTML(HTML)).toBe(res);
+})
+
+test('gets paragraph when main is missing from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <h2>Welcome to Boot.dev</h2>
+        <p>Learn to code by building real projects.</p>
+    </body>
+  </html>`;
+  let res = "Learn to code by building real projects.";
+  expect(getFirstParagraphFromHTML(HTML)).toBe(res);
+})
+
+test('gets paragraph when main is empty from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <h2>Welcome to Boot.dev</h2>
+      <main>
+      </main>
+        <p>Learn to code by building real projects.</p>
+    </body>
+  </html>`;
+  let res = "Learn to code by building real projects.";
+  expect(getFirstParagraphFromHTML(HTML)).toBe(res);
+})
+
+test("getFirstParagraphFromHTML main priority", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <p>Main paragraph.</p>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Main paragraph.";
+  expect(actual).toEqual(expected);
+});
+
+test('returns empty when paragraphs are missing from HTML', () => {
+  let HTML = `<html>
+    <body>
+      <main>
+      </main>
+    </body>
+  </html>`;
+  let res = "";
+  expect(getFirstParagraphFromHTML(HTML)).toBe(res);
 })
 
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,14 +1,42 @@
 import { URL } from "node:url";
+import { JSDOM } from "jsdom";
 
-export function normalizeURL(UrlString: string) {
+export function normalizeURL(UrlString: string): string {
     const url = new URL(UrlString);
-    //console.log(url);
     if (url.pathname.endsWith("/")) {
-        url.pathname = url.pathname.slice(0, url.pathname.length - 1);
+        url.pathname = url.pathname.slice(0, -1);
     }
     if (url.protocol !== "http:" && url.protocol !== "https:") {
         throw Error("Invalid protocol! Only accepts 'http' or 'https'!");
     }
-    //console.log(url.pathname);
     return url.host + url.pathname;
+}
+
+export function getHeadingFromHTML(html: string): string {
+    const dom = new JSDOM(html);
+    const firstHeadings = dom.window.document.querySelectorAll('h1');
+    let text = "";
+    if (firstHeadings.length > 0) {
+        firstHeadings.forEach(h => text += h.textContent + ", ");
+        return text.slice(0, -2);
+    } else {
+        const secondHeadings = dom.window.document.querySelectorAll('h2');
+        secondHeadings.forEach(h => text += h.textContent + ", ");
+        return text.slice(0, -2);
+    }
+}
+
+export function getFirstParagraphFromHTML(html: string): string {
+    const dom = new JSDOM(html);
+    const mainDoc = dom.window.document.querySelector('main');
+    const firstParOutsideMain = dom.window.document.querySelector('p');
+    const fallbackRes = firstParOutsideMain?.textContent !== undefined ?
+        firstParOutsideMain.textContent : "";
+    if (mainDoc !== null) {
+        const firstParagraph = mainDoc.querySelector('p');
+        return firstParagraph?.textContent !== undefined ? firstParagraph?.textContent :
+            fallbackRes;
+    }
+
+    return fallbackRes;
 }
